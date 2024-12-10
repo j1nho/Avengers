06165250585dfb319ae6809e5f5664bcef20f8eb
jinho <j1nh_o@naver.com>
2024-12-10 15:21:38 +0900

    또또 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import './Home.css'
 import Header from "../component/Header";
 import Aside from "../component/Aside";
 import Calendar from "../component/Calendar";
+import profileImg from "../images/profile.png"
 
 const Home = () => {
     return (
@@ -11,42 +12,35 @@ const Home = () => {
             <div className={"Home_container"}>
                 <div className={"Home_wrapper"}>
                     <div className={"Home_Box"}>
-                        <div className={"Home_contents"}>
-                            <div className={"Home_infoBox"}>
-                                <p>User</p>
-                            </div>
-                            <div className={"Home_infoBox"}>
-                                <h2>우리의 자산</h2>
-                                <ul>
-                                    <li><h4>1111-22-333-4444</h4></li>
-                                    <li><h3>3,594,156원</h3></li>
-                                </ul>
-                            </div>
-                            <div className={"Home_infoBox"}>
-                                <h2>예산</h2>
-                                <ul>
-                                    <li><h3>한 달 예산을 세워볼까요?</h3></li>
-                                    <li><span>예산을 설정하고 계획적으로 관리해보세요.</span></li>
-                                </ul>
-                                <div className={"Home_btn"}>
-                                    <button>예산 설정</button>
-                                </div>
+                        <div className={"Home_profileBox"}>
+                            <div className={"Home_profile"}>
+                                <div className={"Home_profileImg"}>
+                                    <img src={profileImg}/>
+                                </div><br/>
+                                <h3>User</h3><br/>
+                                <p><span>함께 관리한지 </span>368일째❤</p><br/>
+                                <h3>목표:매일 10,000원씩 모으기 !0!</h3>
                             </div>
                         </div>
-                        <div className={"Home_contents2"}>
-                            <div className={"Home_infoBox2"}>
-                                <h2>우리의 금융 일정</h2>
-                            </div>
-                            <div className={"Home_infoBox2"}>
-                                <h2>이번 달 지출</h2>
-                                <div className={"circle_box"}>
-                                    <div className={"circle1"}>350,000</div>
+                        <div className={"Home_main"}>
+                            <div className={"Home_contents"}>
+                                <div className={"Home_infoBox"}>
+                                    <h2>우리의 자산</h2>
+                                    <ul>
+                                        <li><h4>1111-22-333-4444</h4></li>
+                                        <li><h3>3,594,156원</h3></li>
+                                    </ul>
+                                </div>
+                                <div className={"Home_infoBox"}>
+                                    <h2>이번 달 지출</h2>
+                                    <div className={"circle_box"}>
+                                        <div className={"circle1"}>350,000</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={"Home_infoBox2"}>
-                                <h2>우리가 이번 달 모은 돈</h2>
-                                <div className={"circle_box"}>
-                                    <div className={"circle2"}>800,000</div>
+                            <div className={"Home_contents2"}>
+                                <div className={"Home_infoBox2"}>
+                                    <h2>우리의 금융 일정</h2>
                                 </div>
                             </div>
                         </div>
