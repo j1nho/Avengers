66e537bd722bbff562d8374818417cd5a720d326
baekhyeran <gpfks0209@naver.com>
2024-12-10 20:33:51 +0900

    모달창

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -4,8 +4,8 @@ function SuccessModal({ onClose }) {
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-                <h2>회원 가입 성공!</h2>
-                <p>이제 로그인 페이지로 이동하세요.</p>
+                <h2>회원 가입 성공 !</h2>
+                <p>로그인 페이지로 이동 합니다.</p>
                 <button onClick={onClose}>확인</button>
             </div>
         </div>
